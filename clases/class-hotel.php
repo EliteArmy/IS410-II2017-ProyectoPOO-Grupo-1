6b6789fe7744c8bd6f2ca0f386acad9b847d0674
EliteArmy <ariel9501@yahoo.com>
2017-10-25 19:56:03 -0600

    cambios

---
+++ clases/class-hotel.php
@@ -0,0 +1,17 @@
+<?php
+
+class Hotel {
+	
+	protected $variable;
+
+	public function __construct(){
+	
+	}
+
+	public function __toString(){
+			return ;
+	}
+
+}
+
+?>
---
+++ clases/class-usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+class Usuario {
+	
+	protected $variable;
+
+	public function __construct(){
+	
+	}
+
+	public function __toString(){
+			return ;
+	}
+
+}
+
+?>
