1d85c51ec189e14dd5992b19df57a1b71136ee6a
denisadonis <denisadonis06@gmail.com>
2017-10-26 19:38:15 -0600

    cambio de nombre en la carpeta clase

---
+++ class/class-hotel.php
@@ -0,0 +1,20 @@
+<?php
+
+class Hotel {
+	
+	protected $Nombre;
+	protected $ubicacion;
+	protected $coordenada; // variable para google maps api
+	protected $numAbitaciones;
+
+	public function __construct(){
+	
+	}
+
+	public function __toString(){
+			return ;
+	}
+
+}
+
+?>
