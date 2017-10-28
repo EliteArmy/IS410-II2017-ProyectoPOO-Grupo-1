94749bf7f26b72c2fe085159cb01f43980877044
denisadonis <denisadonis06@gmail.com>
2017-10-27 22:15:33 -0600

    coordenadas de san pedro

--- js/google-maps.js
+++ js/google-maps.js
@@ -3,7 +3,7 @@ var api = "AIzaSyDRSMtngTTbSi1UdxACNVgpDZyYrEp8Qic";
 var map;
 function initMap() {
   	map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: -34.397, lng: 150.644},
+    	center: {lat: 15.5042923, lng: -88.0187848},
     	zoom: 10
   	});
 }
