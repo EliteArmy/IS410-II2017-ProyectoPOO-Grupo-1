12452d453350ba1b8d61df8ffa0eb83d5deccd21
denisadonis <denisadonis06@gmail.com>
2017-10-28 22:29:48 -0600

    coordenadas san pedro

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,9 +1,29 @@
 var api = "AIzaSyDRSMtngTTbSi1UdxACNVgpDZyYrEp8Qic";
 
-var map;
 function initMap() {
-  	map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: 15.5042923, lng: -88.0187848},
-    	zoom: 10
+	var latLng = {
+		lat: 14.0834956, 
+		lng: -87.1631161
+	};
+
+  	var map = new google.maps.Map(document.getElementById('map'), {
+    	'center': latLng,
+    	'zoom': 14,
+    	'mapTypeId': google.maps.MapTypeId.ROADMAP
+  	});
+
+  	var informacion = new google.maps.InfoWindow({
+  		content: 'UNAH'
+  	});
+
+  	var marker = new google.maps.Marker({
+  		position: latLng,
+  		map: map,
+  		title: 'UNAH',
+  		label: '8.2'
+  	});
+
+  	marker.addListener('click', function () {
+  		informacion.open(map, marker);
   	});
 }
