089446b1c302785bb284262fe38b4984e141e945
EliteArmy <ariel9501@yahoo.com>
2017-11-15 10:10:58 -0600

    hmmmm

--- admin/administradores.php
+++ admin/administradores.php
@@ -83,6 +83,7 @@
                     <li><a href="#">configuraciones</a></li>
                 </ul>
             </div>
+            
             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                 <h1 class="page-header">Usuarios: administradores</h1>
                 <div class="row">
