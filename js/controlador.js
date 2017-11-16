94c78d901730c1df77377ea7c83e63c8f0a4bdf6
EliteArmy <ariel9501@yahoo.com>
2017-11-15 23:57:30 -0600

    Revert "Merge branch 'master' of https://github.com/EliteArmy/IS410-II2017-ProyectoPOO-Grupo-1"
    This reverts commit da0ea951793c8621a32df939d114cf49bd2be9cc, reversing
    changes made to b696bad5d19478d15b86264643050b95eaf646c6.

--- js/controlador.js
+++ js/controlador.js
@@ -2,16 +2,5 @@ $(document).ready(function(){
 	$("#btn-iniciar").click(function () {
 		window.location = "buscar/index.html?buscar=tegucigalpa";
 	});
-
-	$.ajax({
-		url:"ajax/get-info.php?accion=obtener_hoteles",
-		data:"",
-		method:"POST",
-		success:function(resultado){
-			$("#div-hoteles").html(resultado);
-		},
-		error:function(e){
-			alert("Error: " + e);
-		}
-	});
 });
+
